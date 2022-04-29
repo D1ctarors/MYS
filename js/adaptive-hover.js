a10d1f3ad586f360bf0e52e680719cbc982d3ca8
D1ctarors <d1ctators74@yandex.ru>
2022-04-29 22:14:48 +0300

    first commit

---
+++ js/adaptive-hover.js
@@ -0,0 +1,23 @@
+if (/Android|webOS|iPhone|iPad|iPod|Opera Mini/i.test(navigator.userAgent)) {
+
+
+	var touchHover = function () {
+		$('[data-hover]').click(function (e) {
+			e.preventDefault();
+			var $this = $(this);
+			var onHover = $this.attr('data-hover');
+			var linkHref = $this.attr('href');
+			if (linkHref && $this.hasClass(onHover)) {
+				location.href = linkHref;
+				return false;
+			}
+			$this.toggleClass(onHover);
+		});
+	};
+
+
+	$(document).ready(function () {
+		touchHover();
+	});
+
+}
